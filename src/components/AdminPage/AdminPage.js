3871caf79857ebb1833e553daede69b4856f224c
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-09 17:24:19 -0600

    added back to projects button to admin page

--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -4,16 +4,23 @@ import { connect } from 'react-redux';
 //import app bar header from material ui
 import AdminAppBar from '../Material-UI/AdminAppBar';
 import AdminPageForm from '../Material-UI/AdminPageForm';
+import AdminBackToProjectsButton from '../Material-UI/AdminBackToProjectsButton';
 
-//import input form from material ui
 
 
 class AdminPage extends Component {
+  //will send the user back to the projects page when clicked
+  handleClick = () => {
+    console.log('sending user back to projects page');
+    window.location.hash = "";
+  }
+
   // Renders the AdminPage
   render() {
     return (
         <div className="App PageRoutes">
           <AdminAppBar />
+          <AdminBackToProjectsButton />
           <AdminPageForm />
         </div>
       
