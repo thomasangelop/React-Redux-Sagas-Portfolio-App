d958ba88c2c4222ab9760020d7a457bf1d308f97
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-09 16:31:11 -0600

    created material ui appBar header and added it to admin and porfolio pages

--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+//import app bar header from material ui
+import AdminAppBar from '../Material-UI/AdminAppBar';
+
 
 class AdminPage extends Component {
   // Renders the AdminPage
   render() {
     return (
         <div className="App PageRoutes">
+          <AdminAppBar />
         </div>
       
     );
--- src/components/PorfolioPage/PortfolioPage.js
+++ src/components/PorfolioPage/PortfolioPage.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+//import app bar header from material ui
+import PortfolioAppBar from '../Material-UI/PortfolioAppBar';
+
 
 class PortfolioPage extends Component {
   // Renders the PortfolioPage
   render() {
     return (
         <div className="App PageRoutes">
+        <PortfolioAppBar />
         </div>
       
     );
